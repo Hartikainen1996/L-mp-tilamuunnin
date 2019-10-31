eacbad87e7a2a3abd1b8cb3342b5ca919a2a3528
Eemil Hartikainen <55280362+Hartikainen1996@users.noreply.github.com>
2019-10-31 14:42:22 +0200

    Add files via upload

---
+++ KT2Lämpötilamuunnin/src/lämpötilamuunnin.java
@@ -0,0 +1,49 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+	public class lämpötilamuunnin {
+		
+		// Luodaan käyttöliittymä + komponentit. //
+		
+		public static void main(String[] args) {
+			
+			JFrame ikkuna = new JFrame ("Celsius Converter");
+			JLabel tekstikenttä1 = new JLabel("Celcius: ");
+			JTextField syöte = new JTextField();
+			JLabel tekstikenttä2 = new JLabel("Fahrenheit: ");
+			JButton nappi1 = new JButton("Convert");
+			
+			ikkuna.getContentPane().setLayout(new GridLayout(2,2));
+			ikkuna.getContentPane().add(syöte);
+			ikkuna.getContentPane().add(tekstikenttä1);
+			ikkuna.getContentPane().add(tekstikenttä2);
+			ikkuna.setSize(600,125);
+			
+			// Luodaan toiminto napille. //
+			
+			nappi1.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					String tulos = syöte.getText();
+					double tulos1 = Double.parseDouble(tulos);
+					double tulos2 = (tulos1 * 1.8 + 32);
+					tekstikenttä2.setText("Fahrenheit " + tulos2);
+				}
+			});
+			
+			// Lisätään komponentit ikkunaan. //
+			
+			ikkuna.add(tekstikenttä1);
+			ikkuna.add(syöte);
+			ikkuna.add(nappi1);
+			ikkuna.add(tekstikenttä2);	
+			
+			ikkuna.setVisible(true);
+			ikkuna.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+		}
